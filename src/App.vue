6da7e09717fb4c5117341db2301920daaa06be8a
DanielHss <dh99@live.se>
2020-10-03 21:00:31 +0200

    Removing default template code

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Page />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Page from './components/Page.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Page
   }
 }
 </script>
---
+++ src/components/Page.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h2>I am the page</h2>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+h2{
+  color: red;
+}
+</style>
