741f82ea6982d047369f151e247e58de77927b25
DanielHss <dh99@live.se>
2020-10-05 19:23:12 +0200

    Combined Currency + Conditions into Settings.json. Design Start

--- src/components/Checkbox.vue
+++ src/components/Checkbox.vue
@@ -1,19 +1,30 @@
 <template>
-  <md-checkbox v-model="checkboxBool" @change="sendToParent">
-    <h2>{{ name }}</h2>
-  </md-checkbox>
+  <span>
+    <md-checkbox v-model="checkboxBool" @change="sendToParent"> </md-checkbox>
+    <span>{{ formatName }}</span>
+  </span>
 </template>
 
 <script>
 export default {
   props: {
     name: String,
-    bool: Boolean
+    bool: Boolean,
   },
   data() {
     return {
       checkboxBool: this.bool,
     };
+  },
+  computed: {
+    formatName() {
+      return this.name
+        .split("_")
+        .map((part) => {
+          return part.slice(0, 1).toUpperCase() + part.slice(1);
+        })
+        .join(" ");
+    },
   },
   methods: {
     sendToParent() {
@@ -26,5 +37,4 @@ export default {
 };
 </script>
 
-<style>
-</style>
+<style></style>
--- src/components/Page.vue
+++ src/components/Page.vue
@@ -1,39 +1,37 @@
 <template>
   <div>
     <h2>Stormworks Career Settings customizer</h2>
-    <div>
-      <vue-select
-        v-model="selectedPreset"
-        :options="presets"
-        :reduce="(label) => label.value"
-        :clearable="false"
-        :placeholder="'Presets'"
-        @input="updateSettingsFromPreset"
-      />
-    </div>
-    <div>
-      <Checkbox
-        v-for="(value, name, i) in conditions"
-        :key="`${value}` + i"
-        :name="name"
-        :bool="value"
-        @childToParent="onChildClick"
-      />
-    </div>
-    <div>
-      <InputField
-        v-for="(value, name, i) in currency"
-        :key="`${value}` + i"
-        :name="name"
-        :value="value"
-        @childToParent="onChildInput"
-      />
-    </div>
-    <Modal
-      title="Generate Lua File"
-      :settings="conditions"
-      :currency="currency"
-    />
+    <form>
+      <md-list>
+        <div>
+          <vue-select
+            v-model="selectedPreset"
+            :options="presets"
+            :reduce="(label) => label.value"
+            :clearable="false"
+            :placeholder="'Presets'"
+            @input="updateSettingsFromPreset"
+          />
+        </div>
+        <md-list-item
+          v-for="(value, name, i) in conditions"
+          :key="`${value}` + i">
+          <Checkbox :name="name" :bool="value" @childToParent="onChildClick" />
+        </md-list-item>
+        <div>
+          <InputField
+            v-for="(value, name, i) in currency"
+            :key="`${value}` + i"
+            :name="name"
+            :value="value"
+            @childToParent="onChildInput" />
+        </div>
+        <Modal
+          title="Generate Lua File"
+          :settings="conditions"
+          :currency="currency" />
+      </md-list>
+    </form>
   </div>
 </template>
 
@@ -44,8 +42,7 @@ import Modal from "./Modal.vue";
 import vSelect from "vue-select";
 import Vue from "vue";
 import PRESETS_DATA from "../assets/presets.json";
-import CURRENCY_DATA from "../assets/currency.json";
-import CONDITIONS_DATA from "../assets/conditions.json";
+import SETTINGS_DATA from "../assets/settings.json";
 
 export default {
   name: "Page",
@@ -57,18 +54,17 @@ export default {
   },
   data() {
     return {
-      conditionsJson: CONDITIONS_DATA,
-      currencyJson: CURRENCY_DATA,
+      settingsJson: SETTINGS_DATA,
       presetsJson: PRESETS_DATA,
       selectedPreset: "",
     };
   },
   computed: {
     conditions() {
-      return this.conditionsJson;
+      return this.settingsJson.conditions;
     },
     currency() {
-      return this.currencyJson;
+      return this.settingsJson.currency;
     },
     presets() {
       return this.presetsJson;
@@ -77,9 +73,11 @@ export default {
   methods: {
     onChildClick(value) {
       this.updateSettings(value);
+      this.selectedPreset = "Custom";
     },
     onChildInput(value) {
       this.updateCurrency(value);
+      this.selectedPreset = "Custom"
     },
     updateSettings({ field, value }) {
       Vue.set(this.conditions, field, value);
