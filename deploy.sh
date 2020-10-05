52f6d5cc2321a2440f94a1ff4d39e354670dd732
DanielHss <dh99@live.se>
2020-10-05 22:21:18 +0200

    deployment fixed

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,22 @@
 #!/usr/bin/env sh
+
 # abort on errors
 set -e
 
-#build
+# build
 npm run build
 
-#navigate into the build output directory
+# navigate into the build output directory
 cd dist
 
-git init
-git add -abortgit commit -m 'deploy'
+# if you are deploying to a custom domain
+# echo 'www.example.com' > CNAME
 
-git push -f https://github.com/ChocoDream:stormworks-career-setter.git master:gh-pages
+git init
+git add -A
+git commit -m 'deploy'
+
+# if you are deploying to https://<USERNAME>.github.io/<REPO>
+git push -f https://github.com/ChocoDream/stormworks-career-setter.git master:gh-pages
 
 cd -
